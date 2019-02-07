38447296177a4171340f4d6ea58bc3f03e500ab7
Josh Anderson <joshua.frederick.anderson@gmail.com>
2019-02-06 20:11:01 -0800

    second commit

---
+++ bash.js
@@ -0,0 +1,26 @@
+
+// //#1. we output a prompt
+// process.stdout.write('prompt > ');
+//
+// //#2.the stdnin 'data' event triggers after a user types in a line
+// process.stdin.on('data', (data) => {
+//
+// //#3. we use trim to remove \n character at the end of data
+//   const cmd = data.toString().trim();
+//
+//
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt >');
+// });
+
+const commands = require("./commands.js");
+
+//prompt the user for input
+process.stdout.write('prompt > ');
+
+//stdin 'data event triggers after a user types in a line'
+process.stdin.on('data', (userInput) => {
+  userInput = userInput.toString().trim();
+  //evaludateCmd is a function which will be implemented in the commands.js
+  commands.evaludateCmd(userInput);
+});
